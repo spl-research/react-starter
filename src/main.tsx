aea17c39cabe09797832c495dc72d19107cf6044
San Pyae Lin <sanpyae.lin@kbzbank.com>
2024-01-25 16:51:05 +0630

    mantine setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { router } from '@common/constant/routes';
 import { RouterProvider } from '@tanstack/react-router';
 import ReactDOM from 'react-dom/client';
+import '@mantine/core/styles.css';
 
 // Render the app
 ReactDOM.createRoot(document.getElementById('root')!).render(
